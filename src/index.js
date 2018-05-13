b3b4164cabbaa3dc07483e98e6347c5d40932fb2
Tien Pham <r3dsky02@yahoo.com.vn>
2018-05-13 12:47:46 +0300

    Set up Apollo Provider

--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-import './style.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import "./style.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-const GITHUB_BASE_URL = 'https://api.github.com/graphql';
+const GITHUB_BASE_URL = "https://api.github.com/graphql";
 
 const httpLink = new HttpLink({
   uri: GITHUB_BASE_URL,
   headers: {
     authorization: `Bearer ${
       process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
-    }`,
-  },
+    }`
+  }
 });
 
 const cache = new InMemoryCache();
 
 const client = new ApolloClient({
   link: httpLink,
-  cache,
+  cache
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
