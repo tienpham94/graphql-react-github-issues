29dc3dc169117670589271f4dcf2dcfb45c683ee
Tien Pham <r3dsky02@yahoo.com.vn>
2018-05-13 16:32:42 +0300

    Add error handling

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -4,6 +4,7 @@ import { Query } from "react-apollo";
 
 import RepositoryList from "../Repository";
 import Loading from "../Loading";
+import ErrorMessage from '../Error';
 
 const GET_REPOSITORIES_OF_CURRENT_USER = gql`
   {
@@ -39,7 +40,10 @@ const GET_REPOSITORIES_OF_CURRENT_USER = gql`
 
 const Profile = () => (
   <Query query={GET_REPOSITORIES_OF_CURRENT_USER}>
-    {({ data, loading }) => {
+    {({ data, loading, error }) => {
+      if (error) {
+        return <ErrorMessage error={error} />;
+      }
       const { viewer } = data;
 
       if (loading || !viewer) {
